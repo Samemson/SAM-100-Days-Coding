e1753e53e0bfd95d6ed0a3a886b65d62719d4ca5
Samuel Malgwi <99411469+Samemson@users.noreply.github.com>
2023-02-10 23:13:40 +0000

    Day 1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #1. Create a greeting for your program.
-
+print("Welcome to Chari Company name generator.")
 #2. Ask the user for the city that they grew up in.
-
+print("What's your favorite word?")
+word = input()
 #3. Ask the user for the name of a pet.
-
+print("What's your Mother's name?")
+name = input()
 #4. Combine the name of their city and pet and show them their band name.
-
+print("Your Company name could be " + word +" "+ name)
 #5. Make sure the input cursor shows on a new line:
-
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
